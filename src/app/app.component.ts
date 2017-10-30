3778ca1c163649ba5747351eae32f01d090370dc
rajayogan <rajayogan27@gmail.com>
2017-10-30 14:09:48 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  prodname: string;
+  proddesc: string;
+  prodcollection: AngularFirestoreCollection<any> = this.afs.collection('products');
+  prodobs = this.prodcollection.valueChanges();
+
+  constructor(private afs: AngularFirestore) {
+
+  }
+
+  add(){
+    this.prodcollection.add({
+      productname: this.prodname,
+      productdesc: this.proddesc
+    }).then((docRef) => {
+      this.prodcollection.doc(docRef.id).update({
+        prodid: docRef.id
+      })
+    })
+
+      .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  update(prod) {
+    this.prodcollection.doc(prod.prodid).update({
+      productname: 'newprodname'
+    }).then(() => {
+      console.log('updated');
+    })
+  }
+
+  delete(prod) {
+    this.prodcollection.doc(prod.prodid).delete().then(() => {
+      console.log('deleted');
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+
+import { AngularFireModule} from 'angularfire2';
+import { AngularFirestoreModule} from 'angularfire2/firestore';
+import { environment} from '../environments/environment';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +13,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule.enablePersistence()
   ],
   providers: [],
   bootstrap: [AppComponent]
